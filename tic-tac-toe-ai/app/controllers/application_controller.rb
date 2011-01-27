51aaac8b92ad5014dc5a4a731c548d0faa05fb54
L. Preston Sego III <pregosego111_prodigy@yahoo.com>
2011-01-27 16:07:40 -0500

    finally got the CSS to work for the inital screen.

--- tic-tac-toe-ai/app/controllers/application_controller.rb
+++ tic-tac-toe-ai/app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-  
-  def index
-    # ask whether teh player wants to go first or second
-    @player_start_character = ""
-    
-  end
+    protect_from_forgery
+    def index
+        # ask whether teh player wants to go first or second
+
+    end
+
+    def start_game
+      human_is_going_first = params[:human_is_first]
+      
+        if request.xhr?
+            render 'start_game.js.erb', :layout => false
+        end
+    end
+
 end
