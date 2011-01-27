5ad775f4f6016fb864adf7501fff554c3bdfaa08
L. Preston Sego III <pregosego111_prodigy@yahoo.com>
2011-01-27 11:28:44 -0500

    initial addition of rails files

---
+++ tic-tac-toe-ai/app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  
+  def index
+    # ask whether teh player wants to go first or second
+    @player_start_character = ""
+    
+  end
+end
---
+++ tic-tac-toe-ai/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicTacToeAi::Application.config.secret_token = 'ff1d9cd9bba4fa000126feafb560739322c108ac67759b798069d5fc8696313d3bec306e88dd105de79a397a9b439e141c609395aa9edccd8e66bc556fcc4b3f'
